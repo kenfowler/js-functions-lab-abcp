c6d0f8a318f0d6942099cf6a8446ab7db5514c4c
Kenneth Fowler <kenfowler@comcast.net>
2018-01-19 19:11:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidayTo(nameOfHoliday,name) {
   return 'Happy ${nameOfHoliday}, ${name}!'
 }
 function holidayCountdown(holiday,days) {
-  return 'It\'s ${days} days until ${happyHolidays}!'
+  return 'It\'s ${days} days until ${holiday}!'
 }
